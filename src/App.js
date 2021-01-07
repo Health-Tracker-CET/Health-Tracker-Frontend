1dfa37c2e3e259e07ced2d4175df9114a54028de
Adityesh <adityeshmishra98@gmail.com>
2021-01-07 03:43:52 -0500

    Toast and graph working properly

--- src/App.js
+++ src/App.js
@@ -1,53 +1,153 @@
 import './App.css';
-import io from 'socket.io-client';
-import Chart from 'chart.js';
-import { useEffect, useState } from 'react'
-function App() {
-  const [temp, setTemp] = useState([]);
-  useEffect(() => {
-    const ctx = document.getElementById("myChart");
-    new Chart(ctx, {
-      type: "line",
-      data: {
-        labels: ["Temperature"],
-        datasets: [
-          {
-            label: "Body Temperature",
-            data: temp.map(data => data.bodyTemp),
-            backgroundColor: [
-              "Red",
-              "Blue",
-              "Yellow",
-              "Green",
-              "Purple",
-              "Orange"
-            ],
-            borderColor: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-            borderWidth: 1
-          }
-        ]
-      }
-    });
+import {socket} from './socket';
+import React, { useEffect, useState } from 'react';
+import { Line } from '@reactchartjs/react-chart.js'
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+const App = () => {
+  const [maxTemp, setMaxTemp] = useState('');
+  const [maxPulse, setMaxPulse] = useState('');
+  const [data, setData] = useState({
+    labels: [],
+    datasets: [
+      {
+        label: 'Body Temperature',
+        data: [],
+        fill: false,
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgba(255, 99, 132, 0.2)',
+      },
+    ],
   });
 
+  const [pulseData, setPulseData] = useState({
+    labels: [],
+    datasets: [
+      {
+        label: 'Body Pulse',
+        data: [],
+        fill: true,
+        backgroundColor: 'rgb(0,72,186)',
+        borderColor: 'rgb(0,72,186)',
+      }
+    ],
+  });
 
-
-  const socket = io.connect('http://127.0.0.1:5000/');
+  const notify = (message) => {
+    toast.warn(message, {
+      position: toast.POSITION.TOP_RIGHT,
+      autoClose : 3000
+    });
+  };
   
-  socket.on('Temp', (data) => {
-    const arr = temp;
-    arr.push(data);
-    setTemp(arr);
-    console.log(temp.length);
-  })
+  const notifyPulse = (message) => {
+    toast.error(message, {
+      position: toast.POSITION.TOP_RIGHT,
+      autoClose : 3000
+    });
+  }
+
+  
+  
+  const options = {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+          },
+        },
+      ],
+    },
+  }
+
+
+  useEffect(() => {
+    socket.on('Temp', (posts) => {
+      
+      const xAxis = posts.reverse().map((post) => {
+          
+          return new Date(post.createdAt).toLocaleTimeString();
+      })
+      const yAxis = posts.reverse().map(post => {
+        
+        return post.bodyTemp;
+      });
+      let currentMaxTemp = maxTemp;
+      if(Math.max(...yAxis) >= currentMaxTemp) {
+        setMaxTemp(Math.max(...yAxis));
+        
+      }
+
+      const yAxisPulse = posts.reverse().map(post => {
+        return post.bodyPulse;
+      });
+      let currentMaxPulse = maxPulse;
+      if(Math.max(...yAxisPulse) >= currentMaxPulse) {
+        setMaxPulse(Math.max(...yAxisPulse));
+      }
+      
+
+      setData({
+        labels: xAxis,
+        datasets: [
+          {
+            label: 'Body Temperature',
+            data: yAxis,
+            fill: false,
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgba(255, 99, 132, 0.2)',
+          },
+        ],
+      });
+
+      setPulseData({
+        labels: xAxis,
+        datasets: [
+          {
+            label: 'Body Pulse',
+            data: yAxisPulse,
+            fill: false,
+            backgroundColor: 'rgb(0,72,186)',
+            borderColor: 'rgb(0,72,186, 0.2)',
+          },
+        ],
+      });
+
+      
+    })
+
+    return () => {
+      socket.removeAllListeners();
+      socket.disconnect();
+    }
+  },[])
+
+
+  useEffect(() => {
+    if(maxTemp >= 38) {
+      notify("Alert, Body Temp " + maxTemp + '℃');
+    }
+
+    if(maxPulse >= 80) {
+      notifyPulse("Alert, High Body Pulse - " + maxPulse);
+    }
+  }, [maxTemp, maxPulse]);
 
   return (
     <div className="App">
-      <canvas id="myChart" width="200" height="200">
-
-      </canvas>
+      <Line data={data} options={options} />
+      <Line data={pulseData} options={options}/>
+      
+      <ToastContainer/>
+      
+        
+      
     </div>
-  );
+  )
+  
+  
+  
 }
 
 export default App;
