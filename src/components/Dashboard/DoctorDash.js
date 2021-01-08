a8f7bb6918f2104c0a76a61f083cc4f1976281be
Adityesh <adityeshmishra98@gmail.com>
2021-01-08 11:08:47 -0500

    Minor Layout for Doctor Page

--- src/components/Dashboard/DoctorDash.js
+++ src/components/Dashboard/DoctorDash.js
@@ -1,10 +1,70 @@
 import React from 'react';
+import './DoctorDash.css'
+
+const patientList = [
+    {
+        name : 'Adityesh',
+        email : 'adityesh@test.com',
+        age : 22,
+    },
+    {
+        name : 'Adityesh',
+        email : 'adityesh@test.com',
+        age : 22,
+    },
+    {
+        name : 'Adityesh',
+        email : 'adityesh@test.com',
+        age : 22,
+    },
+    {
+        name : 'Adityesh',
+        email : 'adityesh@test.com',
+        age : 22,
+    }
+]
+
 
 
 export default function DoctorDash() {
     return(
-        <div>
-            DashBoard Page
+        <div className="doctor-page">
+            <div className="patients-container">
+                <div className="patient-header">
+                    <p>Patients</p>
+                </div>
+                <div className="patient-table-header">
+                    <ul>
+                        <span>
+                            Name
+                        </span>
+                        <span>
+                            Email
+                        </span>
+                        <span>
+                            Age
+                        </span>
+                    </ul>
+                </div>
+                <div className="patient-list">
+                    {patientList.map(patient => {
+                        return (
+                            <ul>
+                                <span>
+                                    {patient.name}
+                                </span>
+                                <span>
+                                    {patient.email}
+                                </span>
+                                <span>
+                                    {patient.age}
+                                </span>
+                            </ul>
+                        )
+                    })}
+                </div>
+            </div>
+
         </div>
     )
 }
