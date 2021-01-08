bca6420547a373ffa1051436858bde16872f1a75
Adityesh <adityeshmishra98@gmail.com>
2021-01-08 05:46:36 -0500

    Register and Login Page design

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -3,9 +3,12 @@ import { Formik } from 'formik';
 import * as yup from 'yup';
 import { useHistory } from 'react-router-dom';
 import { LoginContext } from '../../context/context';
+import './Login.css';
+import '../Landing.css';
+
 const LoginSchema = yup.object().shape({
-    email: yup.string().email("Not a valid email").required('Email cannot be empty'),
-    password: yup.string().required('Password cannot be empty'),
+    email: yup.string().email("*Not a valid email").required('*Email cannot be empty'),
+    password: yup.string().required('*Password cannot be empty'),
 });
 
 export default function Login() {
@@ -19,8 +22,8 @@ export default function Login() {
 
     return (
         
-                <div>
-                <h3>Login Page for user</h3>
+                <div className="login-page">
+                
                 <Formik
                     validationSchema={LoginSchema}
                     initialValues={{ email: '', password: '' }}
@@ -39,20 +42,24 @@ export default function Login() {
                         resetForm
                     }) => (
 
-                        <form onSubmit={handleSubmit}>
-                            <div>
+                        <form onSubmit={handleSubmit} className="form-container">
+                            <div className="head-container">
+                                <p>User Login</p>
+                            </div>
+                            <div className="input-container">
                                 <input type="email" onChange={handleChange('email')} value={values.email} placeholder="Enter email address" />
                                 <span>{errors.email}</span>
                             </div>
-                            <div>
+                            <div className="input-container">
                                 <input type="password" onChange={handleChange('password')} value={values.password} placeholder="Enter password" />                            <span></span>
                                 <span>{errors.password}</span>
                             </div>
 
-                            <div>
-                                <button type="submit">Login</button>
-                                <button onClick={() => history.push('/register')}>Sign up</button>
-                                <button onClick={() => resetForm()}>Reset</button>
+                            <div className="btn-container">
+                                
+                                <button className="btn register" onClick={() => history.push('/register')}>Sign up</button>
+                                <button className="btn submit" type="submit">Login</button>
+                                <button className="btn reset" onClick={() => resetForm()}>Reset</button>
                             </div>
 
 
--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Formik } from 'formik';
 import * as yup from 'yup';
 import { useHistory } from 'react-router-dom';
+import './Register.css';
+import '../Landing.css';
 
 const RegisterSchema = yup.object().shape({
-    name : yup.string().required("Name cannot be empty"),
-    email: yup.string().email("Not a valid email").required('Email cannot be empty'),
-    password: yup.string().required('Password cannot be empty'),
-    repass :yup.string().oneOf([yup.ref('password'), null], 'Passwords must match').required('Field cannot be empty'),
+    name : yup.string().required("*Name cannot be empty"),
+    email: yup.string().email("*Not a valid email").required('*Email cannot be empty'),
+    password: yup.string().required('*Password cannot be empty'),
+    repass :yup.string().oneOf([yup.ref('password'), null], '*Passwords must match').required('*Field cannot be empty'),
 });
 
 
@@ -19,8 +21,8 @@ export default function Register() {
 
 
     return(
-        <div>
-            <h3>Register Page for user</h3>
+        <div className="login-page">
+            
             <Formik
                 validationSchema={RegisterSchema}
                 initialValues={{ name : '', email: '', password: '', repass : '' }}
@@ -39,29 +41,33 @@ export default function Register() {
                     resetForm
                 }) => (
 
-                    <form onSubmit={handleSubmit}>
-                        <div>
+                    <form onSubmit={handleSubmit} className="form-container">
+                        <div className="head-container">
+                                <p>User Register</p>
+                        </div>
+                        <div className="input-container">
                             <input type="text" onChange={handleChange('name')} value={values.name} placeholder="Enter your name"/>
                             <span>{errors.name}</span>
                         </div>
-                        <div>
+                        <div className="input-container">
                             <input type="email" onChange={handleChange('email')} value={values.email} placeholder="Enter email address"/>
                             <span>{errors.email}</span>
                         </div>
-                        <div>
+                        <div className="input-container">
                             <input type="password" onChange={handleChange('password')} value={values.password} placeholder="Enter password"/>                            <span></span>
                             <span>{errors.password}</span>
                         </div> 
-                        <div>
+                        <div className="input-container">
                             <input type="password" onChange={handleChange('repass')} value={values.repass} placeholder="Re-enter your password"/>                            <span></span>
                             <span>{errors.repass}</span>
                         </div> 
 
                         
-                        <div>
-                            <button type="submit">Register</button>
-                            <button onClick={() => history.push('/login')}>Sign in</button>
-                            <button onClick={() => resetForm()}>Reset</button>
+                        <div className="btn-container">
+                            <button className="btn register" onClick={() => history.push('/login')}>Sign in</button>
+                            <button className="btn submit" type="submit">Register</button>
+                            
+                            <button className="btn reset" onClick={() => resetForm()}>Reset</button>
                         </div>
 
                         
